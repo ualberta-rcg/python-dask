0074c37791c8f14c6025d00a6bf56bab899e92da
Chris Want <cjwant@gmail.com>
2019-07-30 10:58:14 -0600

    Cluster examples.

---
+++ cluster-examples/run-dask-submit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --account=cc-debug
+#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=1024M
+
+export NUM_WORKERS=8
+export THREADS_PER_WORKER=1
+export MEM_PER_WORKER=256mb
+
+# This file (created by the scheduler) communicates Dask network
+# information between the scheduler, workers and the client program
+export SCHEDULER_FILE=${SLURM_JOB_ID}-scheduler.json
+
+source ~/virtualenv/dask/bin/activate
+
+# Start a scheduler and wait for it to spin up
+dask-scheduler --host 127.0.0.1 \
+               --no-dashboard \
+               --scheduler-file $SCHEDULER_FILE &
+sleep 30
+
+# Start a bunch of workers connected to the scheduler and wait
+for worker in `seq $NUM_WORKERS`
+do
+dask-worker --scheduler-file $SCHEDULER_FILE \
+            --no-dashboard \
+            --no-nanny \
+            --nprocs 1 \
+            --nthreads $THREADS_PER_WORKER \
+            --memory-limit $MEM_PER_WORKER &
+done
+sleep 30
+
+# Run python script calling featuretools
+time python run-dask.py
---
+++ cluster-examples/run-dask.py
@@ -0,0 +1,43 @@
+from dask.distributed import Client
+from dask import delayed
+
+import os
+import time
+import socket
+
+def whats_my_number(num):
+    out ='The number on host ' + socket.gethostname() + ' is ' + str(num)
+    time.sleep(2)
+    return out
+
+def report_results(results):
+    out = 'Joining on host ' + socket.gethostname() + '\n'
+    out += '\n'.join(results)
+    return out
+
+# How many seconds since start?
+def elapsed(start):
+    return str(time.time() - start) + ' seconds'
+
+# Connect to the Dask network
+scheduler_file = os.environ['SCHEDULER_FILE']
+client = Client(scheduler_file=scheduler_file)
+
+# Create the task graph
+# 8 delayed calls to what's my number,
+# Assemble output report
+
+data = [1, 2, 3, 4, 5, 6, 7, 8]
+results = []
+for num in data:
+    out = delayed(whats_my_number)(num)
+    results.append(out)
+
+report = delayed(report_results)(results)
+
+# Run it
+time1 = time.time()
+result = report.compute()
+print('Result computed:', elapsed(time1))
+print('\nOutput:')
+print(result)
